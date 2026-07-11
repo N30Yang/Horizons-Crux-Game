3c34743c0081afc5d7274e6d3602a01f593d001d
sDANTOSO <winatsa@whs.school.nz>
2026-07-11 18:34:55 +1000

    Added link between pages

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -9,6 +9,7 @@ func _process(delta: float) -> void:
 
 func _on_start_button_pressed() -> void:
 	print("hello")
+	get_tree().change_scene_to_file("res://Scenes/CharacterSelector.tscn")
 	pass # Replace with function body.
 
 
