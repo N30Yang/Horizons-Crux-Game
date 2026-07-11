3d2338407c207797f797c3dd3b54b1ad2eac2037
sDANTOSO <winatsa@whs.school.nz>
2026-07-11 17:14:33 +1000

    Added character selector

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var selected_character_path: String = "res://Asset 7.png"
+var selected_character_name: String = "Brown"
 
 func _ready() -> void:
 	pass # Replace with function body.
